054c72d0eb6ec5d8f102c8f4df7061b4ec6135ce
SvetlanaIL <ilyasovasv91@yandex.ru>
2021-03-03 11:39:38 +0300

    add some edits

--- src/App.js
+++ src/App.js
@@ -3,43 +3,45 @@ import "./App.css";
 
 function App() {
   const [num, setNum] = useState();
-  const [color, setColor] = useState("#000");
+  const [color, setColor] = useState();
   const [text, setText] = useState("");
 
   const handleChange = (e) => {
     setNum(e.target.value);
     // console.log(e.target.value);
-    };
-    
+  };
+
   const increment = () => {
-    if(num > 19)
-      setText("Достигнуто максимальное значение");
-    else {
+    if (num < 20) {
+      console.log(+num);
       setText("");
-      setNum(num + 1);
+      setNum(+num + 1);
       setColor("green");
     }
   };
 
   const decrement = () => {
-    if (num < 0) {
-      setText("Достигнуто минимальное значение");
-    } else {
-      setText("");
+    if (num > 1) {
       setNum(num - 2);
       setColor("red");
     }
   };
-  
 
-  // useEffect(() => {
-  //   console.log("!!!");
-  // }, [num]);
+  useEffect(() => {
+    if (num > 19) setText("Достигнуто максимальное значение");
+    if (num < 20) setText("");
+    console.log("!!!");
+  }, [num]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <input style={{ color: color }} className="Input" onChange={handleChange} value={num}></input>
+        <input
+          style={{ color: color }}
+          className="Input"
+          onChange={handleChange}
+          value={num}
+        ></input>
         <div>{text}</div>
         <div className="ButtonBlock">
           <button className="Button" onClick={increment}>
