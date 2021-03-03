86b4f668f766b1a0520a42c5fee2b2087c60eb4a
SvetlanaIL <ilyasovasv91@yandex.ru>
2021-03-03 10:16:11 +0300

    add some

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [num, setNum] = useState(3);
+  const [num, setNum] = useState();
+  const [color, setColor] = useState("#000");
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setNum(e.target.value);
+    // console.log(e.target.value);
+    };
+    
+  const increment = () => {
+    if(num > 19)
+      setText("Достигнуто максимальное значение");
+    else {
+      setText("");
+      setNum(num + 1);
+      setColor("green");
+    }
+  };
+
+  const decrement = () => {
+    if (num < 0) {
+      setText("Достигнуто минимальное значение");
+    } else {
+      setText("");
+      setNum(num - 2);
+      setColor("red");
+    }
+  };
+  
+
+  // useEffect(() => {
+  //   console.log("!!!");
+  // }, [num]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <span>Число: {num}</span>
+        <input style={{ color: color }} className="Input" onChange={handleChange} value={num}></input>
+        <div>{text}</div>
         <div className="ButtonBlock">
-          <button className="Button" onClick = {() => setNum(num + 1)}> + </button>
-          <button className="Button" onClick = {() => setNum(num - 2)}> - </button>
+          <button className="Button" onClick={increment}>
+            +
+          </button>
+          <button className="Button" onClick={decrement}>
+            -
+          </button>
         </div>
       </header>
     </div>
